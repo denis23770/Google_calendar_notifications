cac5fa5982e6a90b95259c7b884ba7277b288963
e-denis.94@mail.ru <Den23770@>
2023-11-14 11:23:18 +0300

    добавить описание, создать readme

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ from googleapiclient.discovery import build
 from dotenv import load_dotenv
 import os
 import telebot
-from multiprocessing import Process, Value, Queue
+# from multiprocessing import Process, Value, Queue
+import threading
+from queue import Queue
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 if os.path.exists(dotenv_path):
@@ -17,16 +19,25 @@ token = os.getenv('token')
 myid = os.getenv('myid')
 
 
-class GoogleCalendar:
+class SenderBot:
+    """
+    Получает словарь с событиями, ищет в них события дата которых наступила, отправляет название, описание события в ТГ
+    """
     SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
     FILE_PATH = 'pythonnot-c71dbb5a4c15.json'
 
-    def __init__(self, calendar_id):
+    def __init__(self, calendar_id, myid):
         self.calendar_id = calendar_id
         credentials = service_account.Credentials.from_service_account_file(
             filename=self.FILE_PATH, scopes=self.SCOPES
         )
         self.service = build('calendar', 'v3', credentials=credentials)
+        self.myid = myid
+        self.bot = telebot.TeleBot(token)
+        self.out_bot = telebot.TeleBot(token)
+        self.message = ''
+        self.past_birthdays = []  # список с исключениями событьи др, должен обновляться каждый год первого января.
+        self.past_weekly = []  # список с исключениями событьи недели, должен обновляться каждый понедельник.
 
     def get_calendar_list(self):
         return self.service.calendarList().list().execute()
@@ -41,26 +52,11 @@ class GoogleCalendar:
     def get_events(self):
         return self.service.events().list(calendarId=self.calendar_id).execute()
 
-
-class SenderBot:
-    """
-    Получает словарь с событиями, ищет в них события дата которых наступила, отправляет название, описание события в ТГ
-    """
-
-    def __init__(self, myid):
-        self.myid = myid
-        self.out_bot = telebot.TeleBot(token)
-        self.past_birthdays = []  # список с исключениями событьи др, должен обновляться каждый год первого января.
-        self.past_weekly = []  # список с исключениями событьи недели, должен обновляться каждый понедельник.
-
-
     def send_event_info(self, events_d):
         events = events_d['items']
         for event in events:
             if event['etag'][1:-1] not in self.past_birthdays and event['etag'][1:-1] not in self.past_weekly:
                 self.comparison(event)
-
-
 
     def comparison(self, event):
         date2 = (
@@ -74,12 +70,14 @@ class SenderBot:
                     if (date1.month, date1.day, date1.hour, date1.minute, date1.second) < date2[1:] and event[
                         'etag'] not in self.past_birthdays:
                         self.past_birthdays.append(event['etag'].replace('"', ''))
+                        # self.bot.send_message(myid, text)
                         print(text)
                 else:
                     date1 = self.changing_datetime_format(event['start']['date'])
                     if (date1.month, date1.day, date1.hour, date1.minute, date1.second) < date2[1:] and event[
                         'etag'] not in self.past_birthdays:
                         self.past_birthdays.append(event['etag'].replace('"', ''))
+                        # self.bot.send_message(myid, text)
                         print(text)
             if 'RRULE:FREQ=MONTHLY' in event['recurrence'][0]:
                 if 'dateTime' in event['start']:
@@ -87,12 +85,14 @@ class SenderBot:
                     if (date1.day, date1.hour, date1.minute, date1.second) < date2[2:] and event[
                         'etag'] not in self.past_birthdays:
                         self.past_birthdays.append(event['etag'].replace('"', ''))
+                        # self.bot.send_message(myid, text)
                         print(text)
                 else:
                     date1 = self.changing_datetime_format(event['start']['date'])
                     if (date1.day, date1.hour, date1.minute, date1.second) < date2[2:] and event[
                         'etag'] not in self.past_birthdays:
                         self.past_birthdays.append(event['etag'].replace('"', ''))
+                        # self.bot.send_message(myid, text)
                         print(text)
             if 'RRULE:FREQ=WEEKLY' in event['recurrence'][0]:
                 # BYDAY = MO, WE, FR, TU, TH, SA, SU   Monday Tuesday Wednesday Thursday Friday Saturday Sunday
@@ -103,12 +103,14 @@ class SenderBot:
                         if (date1.hour, date1.minute, date1.second) < date2[3:] and event[
                             'etag'] not in self.past_birthdays:
                             self.past_weekly.append(event['etag'].replace('"', ''))
+                            # self.bot.send_message(myid, text)
                             print(text)
                     else:
                         date1 = self.changing_datetime_format(event['start']['date'])
                         if (date1.hour, date1.minute, date1.second) < date2[3:] and event[
                             'etag'] not in self.past_birthdays:
                             self.past_weekly.append(event['etag'].replace('"', ''))
+                            # self.bot.send_message(myid, text)
                             print(text)
         else:
             if 'dateTime' in event['start']:
@@ -116,13 +118,18 @@ class SenderBot:
                 if (date1.year, date1.month, date1.day, date1.hour, date1.minute, date1.second) < date2 and event[
                     'etag'] not in self.past_birthdays:
                     self.past_birthdays.append(event['etag'].replace('"', ''))
+                    # self.bot.send_message(myid, text)
                     print(text)
+
+
             else:
                 date1 = self.changing_datetime_format(event['start']['date'])
                 if (date1.year, date1.month, date1.day, date1.hour, date1.minute, date1.second) < date2 and event[
                     'etag'] not in self.past_birthdays:
                     self.past_birthdays.append(event['etag'].replace('"', ''))
+                    # self.bot.send_message(myid, text)
                     print(text)
+
     def changing_datetime_format(self, event_datetime):
         if 'T' not in event_datetime:
             event_datetime += 'T00:00:00+00:00'
@@ -144,51 +151,41 @@ class SenderBot:
                 datetime.now().minute) <= 5:
             self.past_weekly = []
 
-    def start_bot(self, loaded_dict):
+    def start_bot(self, flag):
         i = 0
-        for i in range(2):
+        while not flag.is_set():
             i += 1
-            time.sleep(1)  # 240 секунд в релизной версии
+            time.sleep(240)  # 240 секунд в релизной версии
             print(i)
-            self.send_event_info(loaded_dict)
+            self.send_event_info(self.get_events())
             self.zeroing_past_birthdays()
             self.zeroing_past_weekly()
 
+    def telegram_start(self, flag):
 
-# class TelegramBot:
-#     def __init__(self):
-#         self.stop = 0.0
-#
-#     def start(self, n):
-#         bot = telebot.TeleBot(token)
-#
-#         @bot.message_handler(commands=['stop'])
-#         def stop_bot(message):
+        @self.bot.message_handler(commands=['stop'])
+        def stop_bot(message):
+            flag.set()
+            self.bot.send_message(message.chat.id, 'Bot stopped')
 
-#             bot.send_message(message.chat.id, 'Bot stopped')
-#
-#         @bot.message_handler(commands=['start'])
-#         def start_bot(message):
-#
-#             bot.send_message(message.chat.id, 'Bot started')
-#
-#         bot.polling(none_stop=True)
+        @self.bot.message_handler(commands=['start'])
+        def start_bot(message):
+            self.bot.send_message(message.chat.id, 'Bot started')
+
+        self.bot.polling(none_stop=True)
 
 
 if __name__ == '__main__':
     # with open('dict.txt', 'wb') as f:
     #     pickle.dump(loaded_dict, f)
-    with open('dict.txt', 'rb') as f:
-        loaded_dict = pickle.load(f)
+    # with open('dict.txt', 'rb') as f:
+    #     loaded_dict = pickle.load(f)
     notification_message = Queue()
-    # calendar = GoogleCalendar('denis.elers23@gmail.com')
-    # loaded_dict = calendar.get_events()
-    # stgbot = TelegramBot()
-    # p1 = Process(target=stgbot.start, args=(num,))
-    sbot = SenderBot(myid=myid)
-    sbot.start_bot(loaded_dict)
-    # p2 = Process(target=sbot.start_bot, args=(num, loaded_dict, notification_message))
-    # p1.start()
-    # p2.start()
-    # p1.join()
-    # p2.join()
+    stop_flag = threading.Event()
+    sbot = SenderBot(myid=myid, calendar_id='denis.elers23@gmail.com')
+    p1 = threading.Thread(target=sbot.telegram_start, args=(stop_flag,))
+    p2 = threading.Thread(target=sbot.start_bot, args=(stop_flag, ))
+    p1.start()
+    p2.start()
+    p1.join()
+    p2.join()
